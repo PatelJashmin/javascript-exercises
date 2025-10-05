9ec8122ef53f8b6c06521351adceee6374dff4c3
Patel Jashmin Kumar <227741694+PatelJashmin@users.noreply.github.com>
2025-10-05 05:16:07 +0000

    Use built-in methods to meet the requirements

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,11 +1,14 @@
 const reverseString = function(string) {
-    //using bracket notation - brute force
-    let reversedString = "";
-    for(let i = string.length-1; i>=0; i--) {
-        reversedString += string[i];
-    }
-    return reversedString;
+    // //using bracket notation - brute force
+    // let reversedString = "";
+    // for(let i = string.length-1; i>=0; i--) {
+    //     reversedString += string[i];
+    // }
+    // return reversedString;
+    
     // using split to convert it to array and then reverse array and join??
+    let charactersOfString = string.split("");
+    return charactersOfString.reverse().join("");
 };
 
 // Do not edit below this line
