6f5f88203883727c2e0c0471460da38cb6573e95
Patel Jashmin Kumar <227741694+PatelJashmin@users.noreply.github.com>
2025-10-05 06:28:44 +0000

    Use brute force approach to meet the requirements

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+function removeEleUsingIndex(array, index) {
+    array.splice(index, 1);
+}
+
+const removeFromArray = function(array, ...toBeRemoved) {
+    for(const remove of toBeRemoved) {
+        let index = array.indexOf(remove);
+        while (index>=0) {
+            removeEleUsingIndex(array, index);
+            index = array.indexOf(remove);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
