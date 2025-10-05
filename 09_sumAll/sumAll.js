c39f24f7fdb9e96508423941575039b2b37fdb45
Patel Jashmin Kumar <227741694+PatelJashmin@users.noreply.github.com>
2025-10-05 07:02:04 +0000

    Meet requirements of all test cases

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+function arePositiveIntgers(a, b) {
+    return Number.isInteger(a) && Number.isInteger(b) && a>=0 && b >=0;
+}
 
+const sumAll = function(a, b) {
+    if(!arePositiveIntgers(a,b)) return "ERROR";
+    
+    const min =  Math.min(a,b);
+    const max = Math.max(a,b);
+    let sum = 0;
+    for(let i = min; i<=max; i++) {
+        sum += i;
+    }
+    return sum;   
 };
 
 // Do not edit below this line
