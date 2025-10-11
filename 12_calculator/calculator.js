ef24833019995d70e5fd6cdd24ad2736d2fea0d9
Patel Jashmin Kumar <227741694+PatelJashmin@users.noreply.github.com>
2025-10-11 17:11:32 +0000

    Meet requirements of all test cases

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, currentValue) => {return total += currentValue}, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, currentValue) => {return total *= currentValue}, 1);
 };
 
-const power = function() {
-	
+const power = function(a , b) {
+  let prod = 1;
+	for(let i = 0; i<b; i++) {
+    prod *= a;
+  }
+  return prod;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if(a == 0) return 1;
+  let fact = 1;
+  for(let i = 1; i<=a; i++) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
