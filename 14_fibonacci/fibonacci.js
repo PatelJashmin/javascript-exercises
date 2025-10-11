da7a0ef1bed33d2012018f6aeeb620eb6011aa9d
Patel Jashmin Kumar <227741694+PatelJashmin@users.noreply.github.com>
2025-10-11 18:10:05 +0000

    Handle strings, negative input

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let num = +n;
+    if(num<0) return "OOPS";
+    if(num == 0 || num == 1) return num;
+    let a = 0, b = 1, c;
+    for(let i = 2; i<=num; i++){
+        c = b+a;
+        a = b;
+        b = c;
+    }
+    return b;
 };
 
 // Do not edit below this line
