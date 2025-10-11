c7f8dd1d6c00782d197d44af378efec7271b95b1
Patel Jashmin Kumar <227741694+PatelJashmin@users.noreply.github.com>
2025-10-11 18:01:06 +0000

    Compare reversed string after cleaning it

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+function cleanTheString(string) {
+    return string.toLowerCase().replace(/[^a-z0-9]/g, "");
+}
 
+const palindromes = function (str) {
+    let cleanedString = cleanTheString(str);
+    let revStr = cleanedString.split("").reverse().join("");
+    return cleanedString === revStr;
 };
 
 // Do not edit below this line
